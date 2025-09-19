7a061fa49cabdabe945f027bad7b5c33c17b6fa3
aaawosjdjdi <1115213530@qq.com>
2025-09-19 10:40:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-weight = float(input())
-for year in range(11):
-    earth = weight + year * 0.5
-    moon = earth * 0.165
-    print(f"{year}\t{earth:.2f}\t{moon:.2f}")
+try:
+    weight = float(input())
+except:
+    exit()
+yearly_gain = 0.5
+moon_ratio = 0.165
+for year in range(0, 11):
+    earth_weight = weight + (year * yearly_gain)
+    moon_weight = earth_weight * moon_ratio
+    print(f"{year}\t{earth_weight:.2f}\t{moon_weight:.2f}")
+
