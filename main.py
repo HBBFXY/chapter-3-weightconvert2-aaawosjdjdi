a1ddefec184088082e652d9f4605771e3586509b
aaawosjdjdi <1115213530@qq.com>
2025-09-19 10:49:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 initial_weight = float(input())
-for year in range(1,11):
-    earth_weight = intiatl_weight + year * 0.5
+for year in range(1, 11):
+    earth_weight = initial_weight + 0.5 * year
     moon_weight = earth_weight * 0.165
-    print(f"{year}{earth_weight:.1f}{moon_weight:.3f}")
+    print(f"{year} {earth_weight:.1f} {moon_weight:.3f}")
